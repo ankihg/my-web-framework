8e4c70e3a7fcea83631fd2a6ee0b71e376905906
ankihg <hagelin.annika@gmail.com>
2016-03-11 15:03:59 -0800

    keep speciess unique

--- routes/treeRouter.js
+++ routes/treeRouter.js
@@ -5,6 +5,8 @@ var treeRouter = module.exports = new Router();
 var Species = require(__dirname + '/../lib/species.js');
 
 treeRouter.get('/trees', (req, res) => {
+  console.log('GET request for '+req.url);
+
   var id = req.url.split('/')[2];
   console.log(id);
 
@@ -32,15 +34,23 @@ treeRouter.get('/trees', (req, res) => {
 });
 
 treeRouter.post('/trees', (req, res) => {
+  console.log('POST request for '+req.url);
+
+  var id = req.headers.name.replace(" ", "_");
 
   fs.readFile(__dirname + '/../data/speciess.json', (err, data) => {
     if (err) return treeRouter.respondErr(res, 500, "error reading from species.json");
     var speciess = JSON.parse(data).speciess;
-    var species = {"id":req.headers.name.replace(" ", "_")}
+
+    //keep speciess unique
+    if (speciess.filter((s) => s.id === id).length) return treeRouter.respond(res, 200);
+
+    var species = {"id":id}
     speciess.push(species);
     console.log({"speciess":speciess});
 
     fs.writeFile(__dirname + '/../data/speciess.json', JSON.stringify({"speciess":speciess}), (err) => {
+      console.log(`WRITE ${id} to speciess.json`);
       if (err) return treeRouter.respondErr(res, 500, "error writing to species.json");
       return treeRouter.respond(res, 200);
     });
--- test/server-test.js
+++ test/server-test.js
@@ -16,26 +16,37 @@ describe('server testing', () => {
     });
   });
 
-  // it('should post cunninghamia lanceolata', (done) => {
-  //   request('localhost:3000')
-  //   .post('/trees')
-  //   .set({'name':'cunninghamia lanceolata'})
-  //   .end((err, res) => {
-  //     expect(err).eql(null);
-  //     expect(res).status(200);
-  //     done();
-  //   });
-  // });
-  //
-  // it('should post cedrus deodara', (done) => {
-  //   request('localhost:3000')
-  //   .post('/trees')
-  //   .set({'name':'cedrus deodara'})
-  //   .end((err, res) => {
-  //     expect(err).eql(null);
-  //     expect(res).status(200);
-  //     done();
-  //   });
-  // });
+  it('should post cunninghamia lanceolata', (done) => {
+    request('localhost:3000')
+    .post('/trees')
+    .set({'name':'cunninghamia lanceolata'})
+    .end((err, res) => {
+      expect(err).eql(null);
+      expect(res).status(200);
+      done();
+    });
+  });
+
+  it('should post cedrus deodara', (done) => {
+    request('localhost:3000')
+    .post('/trees')
+    .set({'name':'cedrus deodara'})
+    .end((err, res) => {
+      expect(err).eql(null);
+      expect(res).status(200);
+      done();
+    });
+  });
+
+  it('should post cedrus deodara', (done) => {
+    request('localhost:3000')
+    .post('/trees')
+    .set({'name':'cedrus atlantica'})
+    .end((err, res) => {
+      expect(err).eql(null);
+      expect(res).status(200);
+      done();
+    });
+  });
 
 });
