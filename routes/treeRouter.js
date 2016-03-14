707fd4cc2f2dd1682cf0e001b065eb30430c2425
ankihg <hagelin.annika@gmail.com>
2016-03-14 09:38:27 -0700

    treeRouter cleanup

--- routes/treeRouter.js
+++ routes/treeRouter.js
@@ -10,12 +10,14 @@ treeRouter.get('/speciess', (req, res) => {
   console.log('GET request for '+req.url);
 
   var id = req.url.split('/')[2];
+
   treeRouter.fm.read('speciess', id, Species, (err, fileReport) => {
       if (err) return treeRouter.respondErr(res, 500, fileReport.msg);
       if (fileReport.delivery) return treeRouter.respond(res, 200, `<h1>${fileReport.delivery.name()}</h1>`);
       if (fileReport.all) return treeRouter.respond(res, 200, `<h2>${treeRouter.plz.listAll(fileReport.all)}</h2>`);
   });
 });
+
 
 treeRouter.post('/speciess', (req, res) => {
   console.log('POST request for '+req.url);
@@ -25,6 +27,7 @@ treeRouter.post('/speciess', (req, res) => {
   });
 });
 
+
 treeRouter.put('/speciess',(req, res) => {
   console.log('PUT request for '+req.url);
   var id = req.url.split('/')[2];
@@ -33,8 +36,8 @@ treeRouter.put('/speciess',(req, res) => {
     if (err) return treeRouter.respondErr(res, 500, fileReport.msg);
     treeRouter.respond(res, fileReport.status);
   });
-
 });
+
 
 treeRouter.del('/speciess', (req, res) => {
   console.log('DEL request for '+req.url);
@@ -45,29 +48,8 @@ treeRouter.del('/speciess', (req, res) => {
     if (err) return treeRouter.respondErr(res, 500, fileReport.msg);
     treeRouter.respond(res, fileReport.status);
   });
-
 });
 
-// treeRouter.del('/speciess', (req, res) => {
-//   console.log('DEL request for '+req.url);
-//   var id = req.url.split('/')[2];
-//   if (!id) return treeRouter.respond(res, 200);
-//
-//   fs.readFile(__dirname + '/../data/speciess.json', (err, data) => {
-//     if (err) return treeRouter.respondErr(res, 500, 'error reading from species.json');
-//     var speciess = JSON.parse(data).speciess.map((d) => new Species(d));
-//
-//     //remove species with id from speciess
-//     speciess.forEach((species, i, arr) => { if (species.id === id) speciess.splice(i, 1);  });
-//
-//     fs.writeFile(__dirname + '/../data/speciess.json', JSON.stringify({"speciess":speciess}), (err) => {
-//       if (err) return treeRouter.respondErr(res, 500, 'error writing to species.json');
-//       console.log(`DELETE ${id} from speciess.json`);
-//       return treeRouter.respond(res, 200);
-//     });
-//
-//   });
-// });
 
 treeRouter.get('/trees', (req, res) => {
   console.log('GET request for '+req.url);
@@ -78,7 +60,6 @@ treeRouter.get('/trees', (req, res) => {
       if (fileReport.delivery) return treeRouter.respond(res, 200, '<h1>'+fileReport.delivery.name()+'</h1>');
       if (fileReport.all) return treeRouter.respond(res, 200, `<h2>${treeRouter.plz.listAll(fileReport.all)}</h2>`);
   });
-
 });
 
 
@@ -90,6 +71,7 @@ treeRouter.post('/trees', (req, res) => {
   });
 });
 
+
 treeRouter.put('/trees',(req, res) => {
   console.log('PUT request for '+req.url);
 
@@ -99,8 +81,8 @@ treeRouter.put('/trees',(req, res) => {
     if (err) return treeRouter.respondErr(res, 500, fileReport.msg);
     treeRouter.respond(res, fileReport.status);
   });
-
 });
+
 
 treeRouter.del('/trees', (req, res) => {
   console.log('DEL request for '+req.url);
@@ -111,26 +93,4 @@ treeRouter.del('/trees', (req, res) => {
     if (err) return treeRouter.respondErr(res, 500, fileReport.msg);
     treeRouter.respond(res, fileReport.status);
   });
-
 });
-
-// treeRouter.del('/trees', (req, res) => {
-//   console.log('DEL request for '+req.url);
-//   var id = req.url.split('/')[2];
-//   if (!id) return treeRouter.respond(res, 200);
-//
-//   fs.readFile(__dirname + '/../data/trees.json', (err, data) => {
-//     if (err) return treeRouter.respondErr(res, 500, 'error reading from trees.json');
-//     var trees = JSON.parse(data).trees.map((d) => new Tree(d));
-//
-//     //remove species with id from speciess
-//     trees.forEach((tree, i, arr) => { if (tree.id === id) arr.splice(i, 1);  });
-//
-//     fs.writeFile(__dirname + '/../data/trees.json', JSON.stringify({"trees":trees}), (err) => {
-//       if (err) return treeRouter.respondErr(res, 500, 'error writing to trees.json');
-//       console.log(`DELETE ${id} from trees.json`);
-//       return treeRouter.respond(res, 200);
-//     });
-//
-//   });
-// });
